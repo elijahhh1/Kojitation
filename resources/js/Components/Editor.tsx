98171f5b9fe705489749690cd3669735aa97536d
Vince <vince.sanity123@gmail.com>
2023-10-28 23:00:48 +0800

    Editor Update

--- resources/js/Components/Editor.tsx
+++ resources/js/Components/Editor.tsx
@@ -1,6 +1,15 @@
 import React, { FC, useEffect, useMemo, useState } from 'react';
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import { BlockNoteView, useBlockNote,
+    FormattingToolbarPositioner,
+    HyperlinkToolbarPositioner,
+    SideMenuPositioner,
+    SlashMenuPositioner,
+    ToggledStyleButton,
+    Toolbar,
+    ToolbarButton,
+    useEditorContentChange,
+    useEditorSelectionChange, } from "@blocknote/react";
 import "@blocknote/core/style.css";
 import { useTheme } from './Providers/ThemeProvider';
 import { Document } from '@/types';
@@ -23,9 +32,67 @@ const Editor:FC<EditorProps> = ({onChange,document,editable}) => {
     
     return (
         <div>
-            <BlockNoteView editor={editor} theme={theme==='dark'? 'dark':'light'}  />
+            <BlockNoteView editor={editor} theme={theme==='dark'? 'dark':'light'}  >
+                <FormattingToolbarPositioner
+                        editor={editor}
+                        formattingToolbar={CustomFormattingToolbar}
+                    />
+                <HyperlinkToolbarPositioner editor={editor} />
+                <SlashMenuPositioner editor={editor} />
+                <SideMenuPositioner editor={editor} />
+            </BlockNoteView>
         </div>
     )
 }
 
-export default Editor
+export default Editor;
+
+
+
+const CustomFormattingToolbar = (props: { editor: BlockNoteEditor }) => {
+    // Tracks whether the text & background are both blue.
+    const [isSelected, setIsSelected] = useState<boolean>(
+    props.editor.getActiveStyles().textColor === "blue" &&
+        props.editor.getActiveStyles().backgroundColor === "blue"
+    );
+
+    // Updates state on content change.
+    useEditorContentChange(props.editor, () => {
+    setIsSelected(
+        props.editor.getActiveStyles().textColor === "blue" &&
+        props.editor.getActiveStyles().backgroundColor === "blue"
+    );
+    });
+
+    // Updates state on selection change.
+    useEditorSelectionChange(props.editor, () => {
+    setIsSelected(
+        props.editor.getActiveStyles().textColor === "blue" &&
+        props.editor.getActiveStyles().backgroundColor === "blue"
+    );
+    });
+
+    return (
+    <Toolbar>
+        {/*Default button to toggle bold.*/}
+        <ToggledStyleButton editor={props.editor} toggledStyle={"bold"} />
+        {/*Default button to toggle italic.*/}
+        <ToggledStyleButton editor={props.editor} toggledStyle={"italic"} />
+        {/*Default button to toggle underline.*/}
+        <ToggledStyleButton editor={props.editor} toggledStyle={"underline"} />
+        <ToggledStyleButton editor={props.editor} toggledStyle={"strike"} />
+        {/*Custom button to toggle blue text & background color.*/}
+        <ToolbarButton
+        mainTooltip={"Blue Text & Background"}
+        onClick={() => {
+            props.editor.toggleStyles({
+            textColor: "blue",
+            backgroundColor: "blue",
+            });
+        }}
+        isSelected={isSelected}>
+        Blue
+        </ToolbarButton>
+    </Toolbar>
+    );
+};
