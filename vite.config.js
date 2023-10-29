3b1699ed29cefb9540dcb5394fb742f3c05b977e
Vince <vince.sanity123@gmail.com>
2023-10-30 00:46:23 +0800

    Vite Fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: ['resources/js/app.tsx','resources/js/Pages/Welcome.tsx',],
             refresh: true,
         }),
         react(),
