e73187ebd6415179abdedc67ddf0de2b27d7e101
Lester Ace Antipino <lacennister@gmail.com>
2023-12-09 01:14:52 +0800

    Update

--- app/Http/Controllers/Auth/ResetCodeController.php
+++ app/Http/Controllers/Auth/ResetCodeController.php
@@ -41,7 +41,10 @@ class ResetCodeController extends Controller
     {
         // Use Carbon to get the current time and subtract 60 minutes
         $one_hour_ago = Carbon::now()->subMinutes(60)->toDateTimeString();
-        $reset_code = PasswordResetCode::with(['user'])->where('code', $request->pin)->where('created_at', '>=', $one_hour_ago)->first();
+        $user = User::where('email', $email)->first();
+        $reset_code = PasswordResetCode::with(['user'])->where('user_id', $user->id)->where('code', $request->pin)
+            ->where('created_at', '<=', $one_hour_ago)
+            ->first();
 
         if (!$reset_code) throw ValidationException::withMessages(['pin' => 'This is either an Invalid Code or the Code has  already expired']);
         return Inertia::render('Auth/PasswordReset', [
